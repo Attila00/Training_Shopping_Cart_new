44870f992a2e43478047d14247df8cbaaebb0a6b
Parth Mittal <parmitta2@publicisgroupe.net>
2021-05-04 17:01:00 +0530

    Initial boilerplate code setup

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../css/main.scss';
+import Image from './atoms/Image';
+const App =()=>{
+    return (
+        <div className="header">
+            <Image source="./static/images/logo.png"/>
+        </div>
+    )
+}
+export default App;
