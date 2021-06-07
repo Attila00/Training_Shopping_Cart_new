04d33ce2a77ee60d10243e5075f74822de2294d7
Attila00 <mparth019@gmail.com>
2021-06-07 14:30:34 +0530

    Files related to Signin updations

--- src/components/Pages/Register/Register.js
+++ src/components/Pages/Register/Register.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import FormTemplate from '../../Templates/FormTemplates/FormTemplate';
 const Register = ({}) =>{
-    let inputList=[{id:"input__firstname", name:"firstname", placeholder:"register.inputs.1", required:true, autofocus:true},
-{id:"input__lastname", name:"lastname", placeholder:"register.inputs.2", required:true, autofocus:false},
-{id:"input__mail", name:"mail", placeholder:"register.inputs.3", required:true, autofocus:false, type:"email"},
-{id:"input__password", name:"password", placeholder:"register.inputs.4", required:true, autofocus:false, type:"password", minlength:8, maxlength:10},
-{id:"input__conpass", name:"conpass", placeholder:"register.inputs.5", required:true, autofocus:false, type:"password", match:{match:true, matchwith:"password"}}];
+    let inputList=[{id:"input__firstname", name:"firstname", placeholder:"register.inputs.1", required:true, autofocus:true, error:""},
+{id:"input__lastname", name:"lastname", placeholder:"register.inputs.2", required:true, autofocus:false, error:""},
+{id:"input__mail", name:"mail", placeholder:"register.inputs.3", required:true, autofocus:false, type:"email", error:""},
+{id:"input__password", name:"password", placeholder:"register.inputs.4", required:true, autofocus:false, type:"password", minlength:1, maxlength:10, error:""},
+{id:"input__conpass", name:"conpass", placeholder:"register.inputs.5", required:true, autofocus:false, type:"password", match:{match:true, matchwith:"password"}, error:""}];
 
     return (            
-            <FormTemplate inputList={inputList} 
+            <FormTemplate inputListUsed={inputList} 
             title="register.title" 
             description="register.desc"
             tabTitle = "headernav.4"/>
--- src/components/Templates/FormTemplates/FormTemplate.js
+++ src/components/Templates/FormTemplates/FormTemplate.js
@@ -1,51 +1,82 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import './FormTemplates.scss';
 import Button from '../../atoms/Button/Button';
 import TextInput from '../../atoms/TextInputField/TextInput';
 import { useTranslation } from 'react-i18next';
-import { updateTitle } from '../../../utils/utils'
-import './FormTemplates.scss';
-const FormTemplate = ({inputList, title, description, tabTitle}) =>{
-    const {t, i18n} = useTranslation();
-    const [inputObject, setInputObject] = React.useState({});
-    React.useEffect(()=>{
+import { updateTitle } from '../../../utils/utils';
+import { GlobalContext } from '../../../Context/globalContext';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+const FormTemplate = ({inputListUsed, title, description, tabTitle}) =>{
+    const {t} = useTranslation();
+    const history = useHistory();
+    const location = useLocation();
+    const [inputList, setInputList] = useState(inputListUsed);
+    const [inputObject, setInputObject] = useState({});
+    const { dispatch } = useContext(GlobalContext);
+    //To set initial input values
+    useEffect(()=>{
         let temp = {};
         inputList.forEach(ele => temp[ele.name] = "");        
         setInputObject(temp);
     },[]);
+
     //Function to update title
     updateTitle(t(tabTitle));
+
+    //To handle success submission of form
+    const successSubmit = () =>{
+        let payload = inputObject['firstname'] ? 
+        {username:inputObject['mail'], name:inputObject['firstname']+inputObject['lastname']} 
+        :{username:inputObject['mail']};
+        dispatch({
+            type:'SIGNED_UP',
+            payload:{loggedIn:true, ...payload}
+        });
+        history.push('/');
+    };
+
+    //To handle onsubmit of form.
     const submitfinal = (e) =>{
-        let matchedinput = inputList.filter(ele => ele?.match?.match == true)[0];
-        if(inputObject[matchedinput.match.matchwith] !== inputObject[matchedinput.name]) alert("Please provide matching values")
+        let temp = inputList;
+        let submit = true;
+        temp.forEach(ele => {
+            if(ele?.match?.match == true)if(inputObject[ele.match.matchwith] !== inputObject[ele.name]){
+                ele['error'] = "providematching";
+                submit = false;
+            }
+        })  
         e.preventDefault();
+        submit ? successSubmit() : setInputList(temp);
     };
     return (
-            <main className="main-section">
-                <section className="main-section_description">
-                    <h2>{t(title)}</h2>
-                    <p className="desc-text">{t(description)}</p>  
-                </section>
-                <section className="main-section_form">
-                <form  onSubmit={submitfinal}>
-                        {inputList && inputList.map(inputitem =>{
-                            return (
-                                <TextInput inputchangehandler={(e) => setInputObject({...inputObject, [e.target.name]:e.target.value})}
-                                name={inputitem.name}
-                                id={inputitem.id}
-                                placeholder={inputitem.placeholder}
-                                value={inputObject[inputList.name]}
-                                required={inputitem.required}
-                                autofocus={inputitem.autofocus}
-                                type={inputitem.type}
-                                minlength={inputitem.minlength}
-                                maxlength={inputitem.maxlength}
-                                error={""}/>
-                        );
-                        })}
-                        <Button reqClass="login-button" type="primary" buttontype="submit">{t(title)}</Button>
-                </form> 
-                </section>
-            </main>
+        <main className="main-section">
+            <section className="main-section_description">
+                <h2>{t(title)}</h2>
+                <p className="desc-text">{t(description)}</p>  
+            </section>
+            <section className="main-section_form">
+            <form onSubmit={submitfinal}>
+                {inputList && inputList.map(inputitem =>{
+                    return (
+                        <TextInput key={inputitem.id} inputchangehandler={(e) => setInputObject({...inputObject, [e.target.name]:e.target.value})}
+                        name={inputitem.name}
+                        id={inputitem.id}
+                        placeholder={inputitem.placeholder}
+                        value={inputObject[inputList.name]}
+                        required={inputitem.required}
+                        autofocus={inputitem.autofocus}
+                        type={inputitem.type}
+                        minlength={inputitem.minlength}
+                        maxlength={inputitem.maxlength}
+                        error={inputitem.error}
+                        />);
+                })}
+                <Button reqClass="login-button" buttontype="submit" buttonclickhandler={submitfinal}>{t(title)}</Button>
+                { location.pathname == '/register' ?<><Link className="main-section_form-message" to='/login'>{t('login.title')}</Link> {t('login.memberalready')}</> 
+                : <>{t('register.newhere')}<Link className="main-section_form-message" to='/register'>{t('register.title')}</Link></>}
+            </form> 
+            </section>
+        </main>
       );
 };
 export default FormTemplate;
