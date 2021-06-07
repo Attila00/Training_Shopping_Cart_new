048b2dfd21ea2c1bd655a5a9dc8e5a5ecd69e68f
Attila00 <mparth019@gmail.com>
2021-06-07 14:33:03 +0530

    Files related to Header and Footer updations

--- src/components/Organisms/Footer/Footer.js
+++ src/components/Organisms/Footer/Footer.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './Footer.scss';
 import Icon from '../../atoms/Icon/Icon';
 import '../../../assets/images/hinditoenglish.svg';
-import {useTranslation} from 'react-i18next';
+import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router';
+import Button from '../../atoms/Button/Button';
 const Footer =()=>{
     const {t, i18n} = useTranslation();
+    const location = useLocation();
+    if(location.pathname.includes('cart')) return null;
 return (
-    <footer tabIndex={0} className="footer-main">
-        <section className="footer-main_subcontainer">
-            <p tabIndex={0}>{t('footer.message')}</p>
-            <Icon tabIndex={0} source="../../../assets/images/hinditoenglish.svg" alt={`Click to translate language between Hindi and English`} reqclass={`imagefitlogin clickable`} onClickHandler={() => i18n.changeLanguage( i18n.language == 'en' ? 'hin' : 'en')}/>
-        </section>
+    <footer className="footer-main">
+        <div className="footer-main_subcontainer">
+            <p>{t('footer.message')}</p>
+            <Button reqClass={`footer-main_subcontainer-image_button`} buttonclickhandler={() => i18n.changeLanguage( i18n.language == 'en' ? 'hin' : 'en')}><Icon source="../../../assets/images/hinditoenglish.svg" alt={`Click to translate language between Hindi and English`} reqclass={`imagefitlogin clickable`}/></Button>
+        </div>
     </footer>
 )
 };
--- src/components/Organisms/header/Header.js
+++ src/components/Organisms/header/Header.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Icon from '../../atoms/Icon/Icon';
 import './Header.scss';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
 import { CartHeader } from '../../molecules/HeaderCard/CartHeader';
+import { GlobalContext } from '../../../Context/globalContext';
+import { isNotUndefined } from '../../../utils/utils';
 export default function Header(){
     let location = useLocation();
     const [selected, setSelected] = React.useState('/');
+    const { globalState:{isUser} } = useContext(GlobalContext);
     // Used for text translation
     const {t} = useTranslation();
     
@@ -28,10 +31,10 @@ export default function Header(){
                 <Link id="header__nav-home-item_products-link" className={`header__nav-home-item_products-link ${selected == "products"?'selected':''}`} to="/products/All">{t('headernav.2')}</Link>
             </nav>
             <div className="header__nav-login-item">
-                <nav className="header__nav-login-item_subheader">
+               {isUser.loggedIn ? <span className="header__nav-login-item_subheader">{t('headernav.hello')}<b>{isNotUndefined(isUser.name) ? isUser.name : isUser.username}</b></span> : <nav className="header__nav-login-item_subheader">
                     <Link className={`header__nav-login-item_subheader_login-link ${selected == "login"?'selected':''}`} to="/login">{t('headernav.3')}</Link>
                     <Link className={`header__nav-login-item_subheader_register-link ${selected == "register"?'selected':''}`} to="/register">{t('headernav.4')}</Link>
-                </nav>
+                </nav>}
                 {/* Header Cart Component */}
                 <CartHeader headerClass={`header__nav-login-item_cart`} />
             </div>
