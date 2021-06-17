7fcb53ecee712c0c98610133109a6d41c0826d76
Attila00 <mparth019@gmail.com>
2021-06-17 12:50:23 +0530

    Optimized functionality and added null check

--- src/components/Pages/Products/Products.js
+++ src/components/Pages/Products/Products.js
@@ -1,13 +1,12 @@
 import React,{useState, useEffect, useContext} from 'react';
 import './Products.scss';
-import Scrolltop from '../../molecules/ScrollTop/Scrolltop';
 import Dropdown from '../../molecules/Productdropdown/Dropdown';
 import CardProduct from '../../molecules/Card-Product/CardProduct';
 import Categories from '../../Organisms/ProductCategories/Categories';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
 import { scrollTop } from '../../../utils/scrollutils';
-import { updateTitle, remSpaces } from '../../../utils/utils';
+import { updateTitle, remSpaces, isNotUndefined} from '../../../utils/utils';
 import { GlobalContext } from '../../../Context/globalContext';
 import { useMediaQuery } from '../../../customHooks/useMediaQuery';
 const Products = (props) =>{
@@ -18,11 +17,18 @@ const Products = (props) =>{
     const isMobile = useMediaQuery('(max-width:480px)');
     const location  = useLocation();
     const {t} = useTranslation();
+
     //To observe url updation as per category selection/de-selection
     useEffect(()=>{
         let filterval = props.location?.selected?.id ? props.location.selected.id : "All";
         handleCategoryFilter(filterval);
     },[location]);
+
+    //To retain filter on refresh
+    useEffect(()=>{
+        let filterva = categories.find(product => remSpaces(product.name) == location.pathname.split("/")[2]);
+        handleCategoryFilter(filterva ? filterva.id : "All");
+    },[]);
   
     //To update title
     updateTitle(t('headernav.2'));
@@ -47,11 +53,13 @@ const Products = (props) =>{
         setDisplayProductList(temp);           
         scrollTop();
     };
+
     //To handle category selec/de-selec and update url as per.
     const updateProductsRoute = (id, name) =>{    
         props.history.push({pathname: `/products/${ selectedCategory !== id ? remSpaces(name): "All"}`
         ,selected: {id}});
     };
+
     return (
     <div className="products">
         {!isMobile ?
@@ -67,7 +75,7 @@ const Products = (props) =>{
             />)
         }
         <section id="products_list" className="products_list">
-            {displayProductList.length > 0 && displayProductList.map(product =>{
+            {isNotUndefined(displayProductList) && displayProductList.length > 0 && displayProductList.map(product =>{
             return <CardProduct key={product.id} details={product}/>
         })}
         </section>
