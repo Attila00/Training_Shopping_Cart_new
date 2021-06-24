3ae0f1ac40719955979652439e1a60f6c8beb146
Attila00 <mparth019@gmail.com>
2021-06-24 12:21:15 +0530

    random code removal

--- src/components/Organisms/Cart-Item/CartItem.js
+++ src/components/Organisms/Cart-Item/CartItem.js
@@ -10,7 +10,7 @@ export const CartItem = ({id, imgSource, name, price, totalPrice, stock, quantit
         <section className="main_cart_container-item_section-details">
             <h3 className="margin-tb-zero">{name}</h3>
             <section className="main_cart_container-item_section-details_section">
-                <Counter minVal={0} maxVal={stock} counterValue={quantity} id={id} totalAmount={totalPrice}/> 
+                <Counter minVal={0} maxVal={stock} counterValue={quantity} id={id}/> 
                 <p className="main_cart_container-item_section-details_section-cost margin-tb-zero">X {price}</p> 
                 <p className="main_cart_container-item_section-details_section-total_cost margin-tb-zero">Rs. {totalPrice}</p>
             </section>
